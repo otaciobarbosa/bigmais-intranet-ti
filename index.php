07229cfb020261ab3ef7bcffcff9b52cfc1042c1
otaciobarbosa <otaciowork@gmail.com>
2025-03-19 15:33:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,90 +1,28 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
-    <title>PAINEL DE MONITORAMENTO</title>
-    <meta http-equiv="refresh" content="10">
-    <style>
-     body {
-            background: linear-gradient(to bottom, #000000, #333333);
-        }
-    </style>
-  </head>
-  <body >
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="pt-br" xml:lang="pt-br">
 
-<?php
-include 'conexao.php';
+<head>
+    <?php include 'custom/header.php'; ?>
+</head>
 
-$sql = "SELECT
-            id,
-            sistema,
-            protocolo,
-            url,
-            porta,
-            CONCAT(protocolo, '://', url, ':', porta) AS rota
-        FROM
-            monitoramento_aplicativos
-        WHERE monitoramento_status = '1'
-         order by status asc";
+<body>
+    <?php include 'custom/navbar.php'; ?>
+    <div class="content-inner">
+        <header class="page-header">
+            <div class="container-fluid">
+                <div class="no-margin-bottom">
+                   
+                </div>
+            </div>
+        </header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
 
-$result = mysqli_query($conn, $sql);
+                </div>
+            </div>
+        </div>
+        <?php include 'custom/footer.php'; ?>
+</body>
 
-if (!$result) {
-    die("Erro na consulta SQL: " . mysqli_error($conn));
-}
-?>
-
-<table class="table table-dark">
-  <thead>
-    <tr>
-      <th style="font-size:25px" cope="col">URL</th>
-      <th style="font-size:25px" cope="col">PORTA</th>
-      <th style="font-size:25px" cope="col">SISTEMA</th>
-      <th style="font-size:25px; text-align:center;" cope="col"><img src='img/cinza.png' style='width:35px;'></th>
-    </tr>
-  </thead>
-  <tbody>
-<?php 
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $response = @file_get_contents($row["rota"]);
-
-        echo "<tr style='font-size:25px'>";
-        echo "<th style='font-size:25px'>".$row["url"]."</th>";
-        echo "<th style='font-size:25px'>".$row["porta"]."</th>";
-        echo "<th style='font-size:25px'>".$row["sistema"]."</th>";
-        echo "<th style='font-size:25px; text-align:center;'>";
-
-        if ($response !== false) {
-            $updateSql = "UPDATE monitoramento_aplicativos SET status = '1' WHERE id = " . $row["id"];
-            if (mysqli_query($conn, $updateSql)) {
-                echo "<img src='img/verde.png' style='width:50px;'></td>";
-            } else {
-                echo "<br><li> Erro ao atualizar o status para a rota do sistema " . $row["sistema"] . ": " . mysqli_error($conn) . PHP_EOL  . "</li>";
-            }
-        } else {
-            $updateSql = "UPDATE monitoramento_aplicativos SET status = '0' WHERE id = " . $row["id"];
-            if (mysqli_query($conn, $updateSql)) {
-                echo "<img src='img/vermelha.png' style='width:50px;'></td>";
-            } else {
-                echo "<br><li> Erro ao atualizar o status para a rota do sistema " . $row["sistema"] . ": " . mysqli_error($conn) . PHP_EOL  . "</li>";
-            }
-        }
-        echo "</th>";
-        echo "</tr>";
-    }
-} else {
-    echo "0 results";
-}
-
-mysqli_close($conn);
-?>
-        </tbody>
-    </table>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
-  </body>
 </html>
